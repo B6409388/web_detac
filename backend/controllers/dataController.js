661b2b586431816253da5f60d4a2ea9c2310cd46
Thuch_o_o <91382929+Thadthon08@users.noreply.github.com>
2024-09-17 20:15:18 +0700

    test 4

--- backend/controllers/dataController.js
+++ backend/controllers/dataController.js
@@ -1,12 +1,22 @@
-const Data = require("../models/schema.js").DataLicent;
+const LicensePlate = require("../models/schema").DataLicent; // เรียกใช้งานโมเดล LicensePlate ที่สร้างไว้
 
+// ฟังก์ชันสร้างและบันทึกข้อมูลใหม่
 exports.createData = async (req, res) => {
   try {
-    const data = new Data(req.body);
+    const { image, lat, long } = req.body; // รับค่าจาก request body
 
-    const newData = await data.save();
-    res.status(201).json(newData);
+    // สร้างเอกสารใหม่จากโมเดล
+    const newData = new LicensePlate({
+      licentplateImg: image,
+      lat: lat,
+      long: long,
+    });
+
+    // บันทึกข้อมูลลงฐานข้อมูล
+    await newData.save();
+
+    res.status(201).json(newData); // ส่งข้อมูลที่บันทึกกลับไปยัง client
   } catch (error) {
-    res.status(400).json({ message: error.message });
+    res.status(400).json({ message: error.message }); // ส่งข้อผิดพลาดกลับไปยัง client
   }
 };
