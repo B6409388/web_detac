833e29790d6bce5ffa5788c8f77ffcfc1c00e858
Thuch_o_o <91382929+Thadthon08@users.noreply.github.com>
2024-09-17 18:54:46 +0700

    add open camera

---
+++ frontend/src/assets/components/camera.jsx
@@ -0,0 +1,30 @@
+import React, { useRef, useState } from "react";
+import Webcam from "react-webcam";
+
+const CameraComponent = () => {
+  const webcamRef = useRef(null);
+  const [imageSrc, setImageSrc] = useState(null);
+
+  const capture = () => {
+    if (webcamRef.current) {
+      const imageSrc = webcamRef.current.getScreenshot();
+      setImageSrc(imageSrc);
+    }
+  };
+
+  return (
+    <div>
+      <Webcam
+        audio={false}
+        ref={webcamRef}
+        screenshotFormat="image/jpeg"
+        width={320}
+        height={240}
+      />
+      <button onClick={capture}>ถ่ายรูป</button>
+      {imageSrc && <img src={imageSrc} alt="ถ่ายรูป" />}
+    </div>
+  );
+};
+
+export default CameraComponent;
