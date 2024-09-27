811395141cbad1721520a686747e16d389d8877c
B6409388 <theerabeem@gmail.com>
2024-09-27 15:06:31 +0700

    11

--- backend/routes/licentplate.js
+++ backend/routes/licentplate.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const dataController = require("../controllers/dataController");
 
 router.post("/", dataController.createData);
+router.get("/", dataController.getAllData);
+router.get("/id", dataController.getDataById);
 
 module.exports = router;
